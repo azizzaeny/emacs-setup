c14aa516528a87f26295ea4ff47e049bda1192a3
azizzaeny <zie.azlance@gmail.com>
2025-03-10 00:34:29 +0700

    update

--- init.el
+++ init.el
@@ -60,6 +60,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(package-selected-packages
    '(websocket company restclient multiple-cursors yaml-mode yasnippet expand-region poly-markdown polymode aggressive-indent php-mode js2-mode python-mode auto-complete counsel clojure-mode cmake-mode)))
 
